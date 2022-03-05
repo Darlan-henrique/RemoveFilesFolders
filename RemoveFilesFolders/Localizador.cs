bc2df7bc989dc2c69518807f93967886ec1215f6
henrique.silva <henrique.silva@LO-IOB0054-NB>
2022-03-05 16:30:29 -0300

    Add project files.

---
+++ RemoveFilesFolders/Localizador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace RemoveFilesFolders
+{
+
+    public class Localizador
+
+    {
+        public int opcao;
+        public string nomeDiretorio;
+        
+        public static void ProcurarPor(string nomeDiretorio, int opcao)
+        {
+            DirectoryInfo diretorio = new DirectoryInfo(nomeDiretorio);
+
+            Console.WriteLine($"Exibindo os resultados do diretorio..... { nomeDiretorio}");
+            Console.WriteLine();
+
+            if (diretorio.Exists)
+            {
+               
+                foreach (DirectoryInfo subDiretorio in diretorio.GetDirectories())
+                {
+                    TimeSpan tempo = DateTime.Now.Subtract(subDiretorio.CreationTime);
+
+                    switch (opcao) {
+                    
+                        case 1:
+                            Console.WriteLine($"Pasta {subDiretorio.Name} criado a {tempo.Days} dias");
+                            break;
+
+                        case 2:
+                            if (tempo.Days >= 30)
+                                subDiretorio.Delete(true);
+                                Console.WriteLine($"Pastas com mais de 30 dias deletadas com sucesso!! ");                           
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Escolha uma opção valida");
+                            break;
+                    }
+                    
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Diretorio nao existe");
+            }
+        }
+        
+    }
+}
---
+++ RemoveFilesFolders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RemoveFilesFolders
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Localizador localizadorDir = new Localizador();
+
+            Console.WriteLine("###########  Procurando diretórios ################\n");
+
+            Console.WriteLine(@"Informe o nome do diretório de início (Ex: c:\dados) ");
+            localizadorDir.nomeDiretorio = Console.ReadLine();
+            Console.WriteLine();
+
+            while (localizadorDir.opcao != 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Selecione a opcao desejada:");
+                Console.WriteLine(
+                    "1: Exibir pastas\n" +
+                    "2: Deletar pastas com + de 30 dias\n" +
+                    "3: Sair");
+                localizadorDir.opcao = int.Parse(Console.ReadLine()); 
+                Localizador.ProcurarPor(localizadorDir.nomeDiretorio, localizadorDir.opcao);               
+            }
+
+        }
+
+    }
+
+}
