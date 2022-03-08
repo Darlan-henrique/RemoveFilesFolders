5355646caead644d34a76069f269564ceab60420
Henrique Darlan <henrique.darlan@hotmail.com>
2022-03-08 20:03:39 -0300

    Primeira atualizacacao!

--- RemoveFilesFolders/Localizador.cs
+++ RemoveFilesFolders/Localizador.cs
@@ -3,54 +3,52 @@ using System.IO;
 
 namespace RemoveFilesFolders
 {
-
     public class Localizador
 
     {
-        public int opcao;
-        public string nomeDiretorio;
-        
+        public int Opcao;
+        public string NomeDiretorio;
+
         public static void ProcurarPor(string nomeDiretorio, int opcao)
         {
             DirectoryInfo diretorio = new DirectoryInfo(nomeDiretorio);
 
-            Console.WriteLine($"Exibindo os resultados do diretorio..... { nomeDiretorio}");
-            Console.WriteLine();
-
             if (diretorio.Exists)
             {
-               
-                foreach (DirectoryInfo subDiretorio in diretorio.GetDirectories())
+                foreach (DirectoryInfo subDiretorios in diretorio.GetDirectories())
                 {
-                    TimeSpan tempo = DateTime.Now.Subtract(subDiretorio.CreationTime);
+                    TimeSpan tempo = DateTime.Now.Subtract(subDiretorios.CreationTime);
 
-                    switch (opcao) {
-                    
+                    switch (opcao)
+                    {
+
                         case 1:
-                            Console.WriteLine($"Pasta {subDiretorio.Name} criado a {tempo.Days} dias");
+                            Console.WriteLine($"Pasta {subDiretorios.Name} criado a {tempo.Days} dias");
                             break;
 
                         case 2:
                             if (tempo.Days >= 30)
-                                subDiretorio.Delete(true);
-                                Console.WriteLine($"Pastas com mais de 30 dias deletadas com sucesso!! ");                           
+                            {
+                                subDiretorios.Delete(true);
+                                Console.WriteLine($"{subDiretorios.Name} com mais de {tempo.Days} dias deletadas com sucesso!! ");
+                            }
                             break;
                         case 3:
                             Environment.Exit(0);
                             break;
                         default:
-                            Console.WriteLine("Escolha uma opção valida");
+                            Console.WriteLine("Digite uma opção valida");
                             break;
                     }
-                    
+
                 }
-               
+
             }
             else
             {
                 Console.WriteLine("Diretorio nao existe");
             }
         }
-        
+
     }
 }
--- RemoveFilesFolders/Program.cs
+++ RemoveFilesFolders/Program.cs
@@ -7,16 +7,15 @@ namespace RemoveFilesFolders
 
         static void Main(string[] args)
         {
-            
             Localizador localizadorDir = new Localizador();
 
             Console.WriteLine("###########  Procurando diretórios ################\n");
 
             Console.WriteLine(@"Informe o nome do diretório de início (Ex: c:\dados) ");
-            localizadorDir.nomeDiretorio = Console.ReadLine();
+            localizadorDir.NomeDiretorio = Console.ReadLine();
             Console.WriteLine();
 
-            while (localizadorDir.opcao != 3)
+            while (localizadorDir.Opcao != 3)
             {
                 Console.WriteLine();
                 Console.WriteLine("Selecione a opcao desejada:");
@@ -24,8 +23,14 @@ namespace RemoveFilesFolders
                     "1: Exibir pastas\n" +
                     "2: Deletar pastas com + de 30 dias\n" +
                     "3: Sair");
-                localizadorDir.opcao = int.Parse(Console.ReadLine()); 
-                Localizador.ProcurarPor(localizadorDir.nomeDiretorio, localizadorDir.opcao);               
+                localizadorDir.Opcao = int.Parse(Console.ReadLine());
+
+                if (localizadorDir.Opcao == 1 || localizadorDir.Opcao == 2)
+                {
+                    Console.WriteLine($"Exibindo os resultados do diretorio..... { localizadorDir.NomeDiretorio}");
+                }
+
+                Localizador.ProcurarPor(localizadorDir.NomeDiretorio, localizadorDir.Opcao);
             }
 
         }
